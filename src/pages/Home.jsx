001f05028ce6567860bb478ed49f92f2c0afce78
Rafael Eduardo Dreissig <rafaeldreissig@gmail.com>
2025-04-17 09:57:48 -0300

    Ajustando pastas e arquivos principais para iniciar o projeto de maneira mais eficiente e organizada.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Reac from 'react';
+import Example from '../components/Example';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Bem vindo a AD CONTEC</h1>
+            <Example />
+        </div>
+    );
+};
+
+export default Home;
